2776456af4ed4f1946073a8ef0048eb1a937aabb
ednar turmanidze <ednari.turmanidze@gmail.com>
2024-04-08 23:12:31 +0400

    dinamiuri urls gavakete

--- 88dynamic_patters/website/views.py
+++ 88dynamic_patters/website/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("You are in website")
+
+def name(request,id):
+    return HttpResponse(f"Hello,{id}")
