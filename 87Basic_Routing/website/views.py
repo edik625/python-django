1f7a12bca0376c4219936b056cfc0df5c60421e5
ednar turmanidze <ednari.turmanidze@gmail.com>
2024-04-08 22:47:36 +0400

    davamate 88 proeqti

--- 87Basic_Routing/website/views.py
+++ 87Basic_Routing/website/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>this is the website</h1>")
+    return HttpResponse("<h1>i am website</h1>")
 
 def index1(requesr):
     return HttpResponse("<h2>This is the home page</h2>")
