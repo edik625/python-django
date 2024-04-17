c70b277bfe60b1af2e97d2e7b50a2c821f29d0c6
ednar turmanidze <ednari.turmanidze@gmail.com>
2024-04-17 19:57:27 +0400

    add new stuless.css

--- 90multi_Page/multi_page/page/views.py
+++ 90multi_Page/multi_page/page/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index1(request):
-    return render(request, 'page.html')
+    return render(request, 'page/page.html')
 
 
 def home_veb(request):
-    return render(request, 'index.html')
+    return render(request, 'page/index.html')
 
 def about_veb(request): 
-    return render(request, 'index.html')
+    return render(request, 'page/index.html')
 
 def contact_veb(request): 
-    return render(request, 'index.html')
+    return render(request, 'page/index1.html')
