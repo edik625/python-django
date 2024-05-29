3f76951e1764c9fb8ccad3345d1fec45023c41a3
edik625 <ednari.turmanidze@gmail.com>
2024-05-29 20:07:23 +0400

    How the Serializer work,how to convert to json format

--- savarjisho/week14/restapi_app/serializer.py
+++ savarjisho/week14/restapi_app/serializer.py
@@ -1,8 +1,35 @@
 from rest_framework import serializers # type: ignore
 from .models import Book
+from rest_framework.renderers import JSONRenderer
+import io
 
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'      # cxrilshi yvela info wamoigebs
-        # fields = ('title', 'publicated_date')   # shesadzlebia calke
+
+# class BookSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Book
+#         fields = '__all__'      # cxrilshi yvela info wamoigebs
+#         # fields = ('title', 'publicated_date')   # shesadzlebia calke
+
+
+
+class BookModel:
+    def __init__ (self, title, publicated_date, author):
+        self.title = title
+        self.publicated_date = publicated_date
+        self.author = author
+
+
+class BookSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length = 255)
+    publicated_date  = serializers.DateField()
+    author = serializers.IntegerField()
+
+def encode():
+    model = BookModel("New Book", '2023-05-29', 2)
+    model_serialized = BookSerializer(model)
+    print(model_serialized.data, type(model_serialized.data))
+    json = JSONRenderer().render(model_serialized.data)
+    print(json)
+
+
+
--- savarjisho/week14/restapi_app/views.py
+++ savarjisho/week14/restapi_app/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
-from rest_framework import generics
-# from rest_framework.views import APIView
-# # Create your views here.
-import io
-# from rest_framework.response import Response
-from .serializer import BookSerializer
+# from django.shortcuts import render
+# from .serializer import BookSerializer
+# from .models import Book
+# from rest_framework import generics
+# # # Create your views here.
+
+
+
+
+# class BookAPIView(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
 
 from .models import Book
+import io
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.forms import model_to_dict
 
-class BookAPIView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
+class BookAPIView(APIView):
+    def get(self, request):
+        lst = Book.objects.all().values()
+        return Response({'books':list(lst)})
+    
+    def post(self, request):
+        New_book = Book.objects.create(
+            title = request.data['title'],
+            publicated_date = request.data['publicated_date'],
+            author = request.data['author']
+        )
+        return  Response({'book': model_to_dict(New_book)})
